5114a6da550674bf76c0b54bc8de024516be28c8
bbouley <b.bouley@hotmail.co.uk>
2015-12-11 20:35:17 -0700

    updating git changes;

--- soundcloud.js
+++ soundcloud.js
@@ -1,25 +1,42 @@
 
-
-// function playsomesound(url){
-//  var api = 'http://api.soundcloud.com/resolve?url='+url+'&client_id=3a76cf73e430887c16e9897fcf630bcc';
-//  console.log(api);
-//   SC.get('/tracks', {
-//     id: 173067990,
-//   },
-//    function(tracks){
-//     SC.oEmbed(tracks[0].uri, {auto_play : true}, document.getElementById('target'));
-//     console.log(tracks[0].permalink_url);
-//   });
-// }
-
-window.onload = function(){
+// window.onload = function(){
     SC.initialize({
     clientid: '3a76cf73e430887c16e9897fcf630bcc'
     });
-    SC.get(function() {
-    SC.get('/tracks/173067990', function(track) {
-    SC.oEmbed(track.permalink_url, document.getElementById('player'));
-    });
-    });
+//     SC.get(function() {
+//     SC.get('/tracks/173067990', function(track) {
+//     SC.oEmbed(track.permalink_url, document.getElementById('player'));
+//     });
+//     });
 
-};
+// };
+
+// var url = 'https://soundcloud.com/calyxteebee/sawn-off';
+
+// var searchUrl = 'http://api.soundcloud.com/resolve?url='+url+'&client_id=YOUR_CLIENT_ID';
+
+// var id = 0;
+
+// $.ajax({
+//   url: searchUrl,
+//   type: 'GET',
+//   data: {client_id:'3a76cf73e430887c16e9897fcf630bcc'},
+//   dataType:'jsonp',
+//   success : function(data){
+//     var output = data.data;
+//     console.log(data);
+//     id = data.id;
+//     debugger;
+//   }
+// });
+
+window.onload = function(){
+
+  SC.initialize({
+    client_id: '3a76cf73e430887c16e9897fcf630bcc'
+  });
+
+  var track_url = 'https://soundcloud.com/calyxteebee/sawn-off';
+    SC.oEmbed(track_url, { auto_play: true }, document.getElementById('player'));
+
+  };
